79e3475d0e978443c357d960e086c5b7c720784c
S4nop <rshtiger@naver.com>
2019-06-09 16:58:40 +0900

    Add table package

---
+++ app/src/main/java/edu/skku/everycalendar/table/TableRowView.java
@@ -0,0 +1,93 @@
+package edu.skku.everycalendar.table;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.xml.sax.helpers.AttributesImpl;
+
+import edu.skku.everycalendar.R;
+
+public class TableRowView {
+    View sup;
+    int hour = 0;
+    TableRow tr;
+    TextView[] tv;
+
+    TableRowView(View sup, int hour){
+        this.sup = sup;
+        this.hour = hour;
+        tv = new TextView[7];
+    }
+
+    public void makeRow(){
+        TableLayout tb = sup.findViewById(R.id.tbView);
+        tr = new TableRow(sup.getContext());
+        tr.setBackgroundColor(Color.parseColor("#ffe3de"));
+        tr.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1f));
+
+        makeLeft();
+        makeRights();
+        tb.addView(tr);
+    }
+
+    private void makeLeft(){
+        String sText = hour < 10 ? "0" + hour + ":00" : hour + ":00";
+        TextView ltv = makeTextView(sText);
+        tr.addView(ltv);
+        setMargin(ltv);
+    }
+
+    private void makeRights(){
+        for(int i = 0; i < 7; i++){
+            tv[i] = makeTextView("");
+            tv[i].setId(i);
+            tr.addView(tv[i]);
+            setMargin(tv[i]);
+        }
+    }
+
+    private TextView makeTextView(String sText){
+        TextView textView = new TextView(sup.getContext());
+        textView.setText(sText);
+        textView.setBackgroundColor(Color.WHITE);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(18);
+        textView.setPadding(0,0,0,0);
+        textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.MATCH_PARENT));
+
+        return textView;
+    }
+
+    private void setMargin(TextView trv){
+        TableRow.LayoutParams lp = (TableRow.LayoutParams)trv.getLayoutParams();
+        lp.setMargins(3,3,3,3);
+    }
+
+    public int[] getTBLocation(int week){
+        int[] out = new int[2];
+        int[] out2 = new int[2];
+        tv[week].getLocationOnScreen(out);
+        tr.getLocationOnScreen(out2);
+        out[0] -= (out2[0] + 14);
+        out[1] = (int)tr.getY() - 18;
+        return out;
+    }
+
+    public int getTBWidth(int week){
+        return tv[week].getWidth();
+    }
+
+    public int getTBHeight(int week){
+        return tv[week].getHeight();
+    }
+
+}
---
+++ app/src/main/java/edu/skku/everycalendar/table/TableView.java
@@ -0,0 +1,131 @@
+package edu.skku.everycalendar.table;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import edu.skku.everycalendar.R;
+import edu.skku.everycalendar.dataType.TimetableData;
+import edu.skku.everycalendar.functions.ToastMaker;
+
+public class TableView extends ConstraintLayout {
+
+    int stHour, edHour;
+    TableRowView[] trs = new TableRowView[24];
+    ArrayList<TimetableData> events;
+    ArrayList<Button> btnList = new ArrayList<>();
+    Integer stPos;
+    Context context;
+    Toast toast;
+    public TableView(Context context, int stHour, int edHour) {
+        super(context);
+        this.stHour = stHour;
+        this.edHour = edHour;
+        this.context = context;
+        init();
+    }
+
+    public TableView(Context context, ArrayList<TimetableData> events) {
+        super(context);
+        this.events = events;
+        init();
+    }
+
+    public TableView(Context context, String stHour, String edHour, ArrayList<TimetableData> events){
+        super(context);
+        this.events = events;
+        init();
+    }
+
+    public void setEvents(ArrayList<TimetableData> events){
+        this.events = events;
+    }
+
+    private void init() {
+
+        String infService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater li = (LayoutInflater) getContext().getSystemService(infService);
+        View v = li.inflate(R.layout.tableview, this, false);
+        stPos = 9 * 12;
+        //addView(v);
+        for(int i = stHour; i < edHour; i++){
+            TableRowView trv = new TableRowView(v, i);
+            trv.makeRow();
+            trs[i] = trv;
+        }
+
+        addView(v);
+
+        addView(addSchedule(new TimetableData("테스트", "", "", "3", 120, 144, 1)));
+
+    }
+
+    public void addEvents(ArrayList<TimetableData> events){
+        int pos[];
+        pos = trs[stPos / 12].getTBLocation(1);
+        while(pos[0] == 0 && pos[1] == 0) {
+            try {
+                wait(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(events != null)
+            for(TimetableData ttd : events){
+                addView(addSchedule(ttd));
+            }
+    }
+
+
+    private Button addSchedule(TimetableData event){
+        Integer week = Integer.parseInt(event.getWeekDay());
+        Integer stTime = event.getStartTime();
+        Integer edTime = event.getEndTime();
+        Log.d("LOG_STTIME", Integer.toString(stTime));
+        TableRowView targTR = trs[stTime / 12];
+        int pos[];
+        int vWidth, vHeight;
+        pos = targTR.getTBLocation(week);
+        vWidth = targTR.getTBWidth(week);
+        vHeight = targTR.getTBHeight(week);
+
+        return setBtnClickListener(makeButton(stTime, edTime, pos, vWidth, vHeight, event.getIdNum()), event);
+    }
+
+    private Button makeButton(Integer stTime, Integer edTime, int pos[], int vWidth, int vHeight, int clr){
+        Button btnSched = new Button(getContext());
+
+        //btnSched.setText(title);
+        btnSched.setY(pos[1] + vHeight * (stTime % 12) / 12 + 20);
+        btnSched.setX(pos[0] + 14);
+        btnSched.setBackgroundColor(clr);
+        ConstraintLayout.LayoutParams btnLParam = new ConstraintLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+        btnLParam.leftMargin = 0;
+        btnLParam.rightMargin = 0;
+        btnLParam.width = vWidth;
+        btnLParam.height = (edTime - stTime) * vHeight / 12 + 6 * ((edTime - stTime) / 12 - 1);
+        btnSched.setLayoutParams(btnLParam);
+
+        return btnSched;
+    }
+
+    private Button setBtnClickListener(Button btn, final TimetableData event){
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ToastMaker.makeToast(context, event.getName() + (event.getDescript() != null ? "\n" + event.getDescript() : "")
+                        + (event.getPlace() != null ? "\n" + event.getPlace() : ""));
+            }
+        });
+        return btn;
+    }
+}
